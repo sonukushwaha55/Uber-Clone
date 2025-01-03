8b1583735742db28f712c0fe8a55f1f3286e4bdc
sonukushwaha55 <sonukumar1772001@gmail.com>
2025-01-03 22:14:01 +0530

    implement user registration functionality with validation and model setup

---
+++ Backend/routes/user.routes.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const { body } = require('express-validator')
+const userController = require('../controllers/user.controller.js')
+const router = express.Router();
+
+router.post('/register', [
+    body('email').isEmail().withMessage("Invalid Email"),
+    body('fullname.firstname').isLength({min:3}).withMessage("Firstname must have more than 3 character long"),
+    body('password').isLength({min:6}).withMessage("Password must have more than 6 character long")
+],
+userController.registerUser
+)
+
+
+module.exports = router;
