6c71b54f78c43680f6cb5e45f25fe57f19075b4f
sonukushwaha55 <sonukumar1772001@gmail.com>
2025-01-04 22:05:11 +0530

    implement user creation logic

--- Backend/app.js
+++ Backend/app.js
@@ -3,14 +3,18 @@ dotenv.config();
 const express = require('express')
 const cors = require('cors')
 const connectToDb = require('./db/db')
+const userRoutes = require('./routes/user.routes.js')
 
 connectToDb();
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 
 app.get('/', (req, res) =>{
     res.send("hello")
 })
+app.use('/users', userRoutes)
 
 module.exports = app;
