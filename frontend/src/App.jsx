2ceaba2ae9568f060a162675ba1dfe33f98678ef
sonukushwaha55 <sonukumar1772001@gmail.com>
2025-01-24 23:17:19 +0530

    Add CaptainLogout functionality and ConfirmRidePopUp component; implement live tracking and ride details display

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import UserProtectWrapper from './pages/UserProtectWrapper'
 import UserLogout from './pages/UserLogout'
 import CaptainHome from './pages/CaptainHome'
 import CaptainProtectWrapper from './pages/CaptainProtectWrapper'
+import CaptainLogout from './pages/CaptainLogout'
 
 const App = () => {
   return (
@@ -35,6 +36,11 @@ const App = () => {
         <CaptainHome/>
       </CaptainProtectWrapper>
     } />
+       <Route path='/captain/logout' element={
+          <CaptainProtectWrapper>
+            <CaptainLogout />
+          </CaptainProtectWrapper>
+        } />
     </Routes>
     </>
   )
---
+++ frontend/src/components/ConfirmRidePopUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ConfirmRidePopUp = () => {
+  return (
+    <div>
+       ConfirmRidePopUp
+    </div>
+  )
+}
+
+export default ConfirmRidePopUp
